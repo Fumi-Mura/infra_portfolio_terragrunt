7a4859c5cac36e8257938705bc21df72720f3ad0
Fumi-Mura <381704fumi@gmail.com>
2024-10-13 16:29:14 +0900

    Opposite

--- terraform/aws/modules/iam/policy/main.tf
+++ terraform/aws/modules/iam/policy/main.tf
@@ -1,23 +1,30 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = [ "sts:AssumeRole" ]
-    resources = var.resources
+data "aws_iam_policy_document" "this" {
+  dynamic "statement" {
+    for_each = var.policy_statement
+    content {
+      effect        = statement.value.effect
+      actions       = statement.value.actions
+      not_actions   = statement.value.not_actions
+      resources     = statement.value.resources
+      not_resources = statement.value.not_resources
 
-    principals {
-      type        = var.assume_type
-      identifiers = var.assume_identifiers
+      dynamic "condition" {
+        for_each = statement.value.condition # Execute loop only when use condition.
+        content {
+          test     = condition.value.test
+          variable = condition.value.variable
+          values   = condition.value.values
+        }
+      }
     }
   }
 }
 
-resource "aws_iam_role" "this" {
-  name                 = "${var.env}-${var.name}-${var.purpose}-iam-role"
-  assume_role_policy   = data.aws_iam_policy_document.assume_role.json
-  managed_policy_arns  = var.managed_policy_arns
-  max_session_duration = var.max_session_duration
+resource "aws_iam_policy" "this" {
+  name   = "${var.env}-${var.name}-${var.purpose}-iam-policy"
+  policy = data.aws_iam_policy_document.this.json
 
   tags = {
-    Name = "${var.env}-${var.name}-${var.purpose}-iam-role"
+    Name = "${var.env}-${var.name}-${var.purpose}-iam-policy"
   }
 }
--- terraform/aws/modules/iam/policy/variables.tf
+++ terraform/aws/modules/iam/policy/variables.tf
@@ -10,23 +10,19 @@ variable "purpose" {
   type = string
 }
 
-variable "resources" {
-  type = list(string)
-}
-
-variable "assume_type" {
-  type = string
-}
-
-variable "assume_identifiers" {
-  type = list(string)
-}
-
-variable "managed_policy_arns" {
-  type = list(string)
-}
-
-variable "max_session_duration" {
-  type = number
-  default = 3600
+variable "policy_statement" {
+  type = list(object({
+    effect        = string
+    actions       = optional(list(string), [])
+    not_actions   = optional(list(string), [])
+    resource      = optional(list(string), [])
+    not_resources = optional(list(string), [])
+    condition = list(object({
+      test     = string
+      variable = string
+      values   = list(string)
+    }))
+  }))
+  default     = []
+  description = "IAM policy statement list."
 }
